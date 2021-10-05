f06954aa8c8f5cdf3aa8147b47492757b9746621
rdbarker <ryan.dean.barker@gmail.com>
2021-10-04 20:12:10 -0400

    add a form to the html for user input

--- script.js
+++ script.js
@@ -10,11 +10,9 @@ function Book(title,author,date,pages,hasRead){
     this.hasRead = hasRead;
     this.drawRow(); 
 }
-
 Book.prototype.changeReadStatus = function(){
     this.hasRead = !this.hasRead;
 }
-
 Book.prototype.drawRow = function(){
     this.row = libraryTable.insertRow(-1);
     tableOrder.forEach(key=>{
@@ -23,17 +21,16 @@ Book.prototype.drawRow = function(){
         cell.appendChild(text);
     })
 }
-
-Book.prototype.sortSelf = function(){
+Book.prototype.sort = function(){
     libraryTable.appendChild(this.row);
 }
-
-
+Book.prototype.delete = function(library){
+    libraryTable.deleteRow(this.row.rowIndex-1);
+    return library.filter(obj => obj!==this);
+}
 
 let bookOne = new Book("Title1","ath1","1967",246,false);
 let bookTwo = new Book("Title2","ath2","1984",269,true);
 let bookThree = new Book("Title3","ath3","1996",960,true);
 
 myLibrary= [bookOne,bookTwo,bookThree];
-
-//drawLibrary(myLibrary);
